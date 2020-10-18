8721e04f82a33809aa739ae4bcfa6790fa6d2fa8
dong-woogie <com6511@gmail.com>
2020-10-19 05:10:45 +0900

    add auth route, register/local route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import Router from "koa-router";
 import api from "./api";
+import auth from "./auth";
 
 const routes = new Router();
 
 routes.use("/api", api.routes());
+routes.use("/auth", auth.routes());
 
 routes.get("/", (ctx) => {
   ctx.body = "hello world";
